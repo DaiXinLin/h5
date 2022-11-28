09ae76cebaf29d29dd19f88cf39c545b201790f5
daixinlin19 <273978871@qq.com>
2022-11-28 21:12:16 +0800

    feat: 初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,20 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import LoginForm from "../views/LoginForm.vue";
+import HomePage from "../views/HomePage.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "Login",
+    component: LoginForm,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/home",
+    name: "home",
+    component: HomePage,
   },
 ];
 
---
+++ src/views/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- <div class="nomepage_box"> -->
+  <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+    <van-swipe-item>1</van-swipe-item>
+    <van-swipe-item>2</van-swipe-item>
+    <van-swipe-item>3</van-swipe-item>
+    <van-swipe-item>4</van-swipe-item>
+  </van-swipe>
+  <!-- </div> -->
+</template>
+
+<script></script>
+
+<style>
+/* .nomepage_box {
+  height: 100px;
+} */
+.my-swipe .van-swipe-item {
+  color: #fff;
+  font-size: 20px;
+  line-height: 150px;
+  text-align: center;
+  background-color: #39a9ed;
+}
+</style>
