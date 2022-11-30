3e1555512fa682ef8d2ce25b52d755091589c044
daixinlin19 <273978871@qq.com>
2022-11-30 16:42:25 +0800

    feat: 第ans次

---
+++ src/views/PersonalCenter.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="percer">
+    <div class="percer_top">
+      <span>个人中心</span>
+      <div class="percer_top_middle">
+        <div class="shawol"></div>
+        <div class="wenzi">
+          <p>七尾Sam</p>
+          <p>159****5665</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {},
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+.percer span {
+  display: block;
+  padding-top: 20px;
+  text-align: center;
+  font-size: 17px;
+  color: #ffffff;
+}
+.percer .percer_top {
+  height: 300px;
+  width: 100%;
+  background-color: rgba(52, 120, 247, 1);
+}
+.percer_top_middle {
+  font-size: 17px;
+  color: #ffffff;
+  margin: 20px;
+  display: flex;
+}
+.percer_top_middle .shawol {
+  width: 50px;
+  height: 50px;
+  background-color: #eee;
+  border-radius: 40px;
+}
+.percer_top_middle .wenzi {
+  margin-left: 20px;
+}
+</style>
---
+++ src/views/RegisterPage.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="form-wrapper">
+    <van-form size="middle" @submit="onSubmit">
+      <van-field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      />
+      <div class="btn-wrapper">
+        <van-button round block type="info" native-type="submit"
+          >注册</van-button
+        >
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    onSubmit(values) {
+      console.log("submit", values);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.form-wrapper {
+  // background: #efefef;
+  box-sizing: border-box;
+  padding-top: 30px;
+}
+.btn-wrapper {
+  margin: 25px 16px 10px 16px;
+  // height: 100vh;
+}
+</style>
